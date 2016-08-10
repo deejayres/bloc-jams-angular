adc9980bf5b52e50e9d891c64262bd61b905aead
Erin Resso <erinresso@gmail.com>
2016-08-09 22:02:06 -0700

    PlayerBarCtrl, previous button (#12)

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,13 +1,13 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
         
-		/**
-		* @desc placeholder for currently selected song object
-		* @type {Object}
-		*/
-        var currentSong = null;
-		
+        /**
+        * @desc Current album info
+        * @type {Object}
+        */
+        var currentAlbum = Fixtures.getAlbum();
+        
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -22,7 +22,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
             
             currentBuzzObject = new buzz.sound(song.audioURL, {
@@ -30,12 +30,12 @@
                 preload: true
             });
             
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
 		
 		/**
 		* @function playSong
-		* @desc plays song passed as parameter
+		* @desc encapsulates song playing behavior
 		* @param {Object} song
 		*/
 		var playSong = function(song) {
@@ -43,20 +43,66 @@
 			song.playing = true;
 		};
         
+        /**
+        * @function getSongIndex
+        * @desc keeps track of the index of song in list of songs
+        * @param {Object} song
+        * @returns {Number}
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+
+        /**
+        * @desc currently selected song from song list
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
+		
+        /**
+        * @function SongPlayer.play
+        * @desc plays song and updates song info as needed
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
 				playSong(song);
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     playSong(song);
                 }
             }
         };
         
+        /**
+        * @function SongPlayer.pause
+        * @desc pauses song and updates song info as needed
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
+        };
+        
+        /**
+        * @function SongPlayer.previous
+        * @desc selects previous song in song list and plays/pauses accordingly
+        */
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+            
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
         
         return SongPlayer;
